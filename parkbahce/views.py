c12a9a110bf2ab3a448f6369e3b78b1cef97ac93
Cihad GUNDOGDU <cihadgundogdu@gmail.com>
2025-05-29 16:35:02 +0300

    Add layout and park management templates with responsive design and OpenLayers integration
    - Created a new layout template for the ParkBahçe application, incorporating Tailwind CSS and Alpine.js for responsive design and dark mode support.
    - Developed a detailed park view template that includes park information, a map using OpenLayers, and a fullscreen map feature.
    - Implemented a park list template with search and filter options, displaying park details and actions for editing and deleting parks.
    - Added JavaScript functionality for handling map initialization and deletion confirmation for parks.

--- parkbahce/views.py
+++ parkbahce/views.py
@@ -1,3 +1,47 @@
+from django.contrib.gis.geos import GEOSGeometry
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Park
+
+
+def index(request):
+    return render(request, "parkbahce/index.html")
+
+
+def park_list(request):
+    parklar = Park.objects.all()
+    return render(request, "parkbahce/parklar/list.html", {"parklar": parklar})
+
+
+# get by uuid
+def park_detail(request, uuid):
+    park = Park.objects.get(uuid=uuid)
+
+    # Park geometrisini EPSG:5256'dan EPSG:4326'ya dönüştür
+    if park.geom:
+        # Geometriyi 5256 (TUREF) olarak ayarla
+        park.geom.srid = 5256
+        # WGS84 (4326) koordinat sistemine dönüştür
+        park.geom.transform(4326)
+
+    return render(request, "parkbahce/parklar/detail.html", {"park": park})
+
+
+def park_edit(request, uuid):
+    park = Park.objects.get(uuid=uuid)
+    if request.method == "POST":
+        park.name = request.POST.get("name")
+        park.description = request.POST.get("description")
+        park.save()
+        return render(request, "parkbahce/parklar/detail.html", {"park": park})
+    return render(request, "parkbahce/park_edit.html", {"park": park})
+
+
+def park_delete(request, uuid):
+    park = Park.objects.get(uuid=uuid)
+    if request.method == "POST":
+        park.delete()
+        return render(
+            request, "parkbahce/park_list.html", {"parklar": Park.objects.all()}
+        )
+    return render(request, "parkbahce/park_delete.html", {"park": park})
